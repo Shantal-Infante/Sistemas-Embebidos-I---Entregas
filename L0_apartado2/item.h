cab0da701c754a2d527fbdef18a527914d38fe6e
Shantal Infante <15-10724@usb.ve>
2025-01-31 02:04:54 -0400

    commit2

--- L0_apartado2/item.h
+++ L0_apartado2/item.h
@@ -8,4 +8,4 @@ typedef struct
     uint8_t  Pin_entrada;  // 1 byte  -> especifica la instruccion
     uint32_t tiempo;      // 4 bytes -> tiempo de interrupcion
     uint16_t referencia;  // 2 bytes
-} Item; // 7 bytes
+} Item; // 7 bytes 
